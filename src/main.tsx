37f7a7dfe064886a34640a29b98dd37a1708884f
Dhivyakanth <dhivyakanth20@gmail.com>
2026-03-30 12:15:51 +0530

    main

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { initWebVitals } from "./lib/performance";
-
-createRoot(document.getElementById("root")!).render(<App />);
-
-void initWebVitals();
